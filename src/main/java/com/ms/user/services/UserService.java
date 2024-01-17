50b0d98d1e8f4bb222483baf9252a0d964a36ce5
Otávio <otaviooasc@gmail.com>
2024-01-16 21:07:50 -0300

    salvar usuario

---
+++ src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,19 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Transactional
+    public UserModel saveUser(UserModel userModel) {
+        return repository.save(userModel);
+    }
+}
